aa4255584bab955ecdc66860b49f4e2be740c949
MEMCIA <84227329+MEMCIA@users.noreply.github.com>
2022-04-01 19:12:17 +0200

    xxx
    xxx

--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Assets.Scripts.Game
@@ -6,46 +7,144 @@ namespace Assets.Scripts.Game
     class Game
     {
         IBoard _board;
+        int _numberOfPlayers = 2;
+        int _activePlayer = 0;
+        int _availableDistanceToMove = 2;
+        int _distanceInWhichPawnIsNotDeleted = 1;
 
         public Game(IBoard board)
         {
             _board = board;
         }
 
-        public bool MakeMove(IField start, IField target)
+        public void Turn(IField start, IField target)
+        {
+            if(MakeMove(start,target))
+            {
+                SetNextActivePlayer();
+            }
+        }
+
+        void SetNumberOfPlayers(int number)
+        {
+            _numberOfPlayers = number;
+        }
+
+        void SetNextActivePlayer()
+        {
+            int lastPlayer = _numberOfPlayers - 1;
+            if (_activePlayer == lastPlayer)
+            {
+                _activePlayer = 0;
+            }
+            else
+            {
+                _activePlayer++;
+            }
+        }
+
+        bool CheckIfPawnBelongsToCurrentPlayer(IField start)
+        {
+            if (start.Pawn.Owner == _activePlayer) return true;
+            return false;
+        }
+
+        bool MakeMove(IField start, IField target)
         {
             if (start.Pawn == null)
                 return false;
             if (!target.IsEmpty())
                 return false;
             _board.PlacePawnAt(target.Position, start.Pawn.Owner);
+            int distanceBetween = CheckDistanceBetween(start, target);
+            if (!CheckIfStartPawnMustBeDeleted(distanceBetween)) return true;
+            _board.RemovePawn(start.Position);
+            ChangeOwnerOfNeighboringPawns(target);
             return true;
+        }
+
+        void ChangeOwnerOfNeighboringPawns(IField target)
+        {
+            List<IField> fieldWithEnemyPawns = FindEnemiesPawnsInNeighborhood(target);
+
+            foreach (var f in fieldWithEnemyPawns)
+            {
+                f.Pawn.SetOwner(target.Pawn.Owner);
+            }
+        }
+
+        List<IField> FindEnemiesPawnsInNeighborhood(IField target)
+        {
+            List<IField> neighbors = FindFieldsinDistance(target.Position, 1);
+            List<IField> fieldWithEnemyPawns = new List<IField>();
+
+            foreach (var n in neighbors)
+            {
+                if (n == null || n.IsEmpty()) continue;
+                if (n.Pawn == null) continue;
+                if (n.Pawn.Owner == target.Pawn.Owner) continue;
+                fieldWithEnemyPawns.Add(n);
+            }
+
+            return fieldWithEnemyPawns;
+        }
+
+        bool CheckIfStartPawnMustBeDeleted(int distanceBetween)
+        {
+            if (distanceBetween > _distanceInWhichPawnIsNotDeleted) return true;
+            return false;
+        }
+
+        int CheckDistanceBetween(IField start, IField target)
+        {
+            int distanceX = Mathf.Abs(start.Position.x - target.Position.x);
+            int distanceY = Mathf.Abs(start.Position.y - target.Position.y);
+            return Mathf.Max(distanceX, distanceY);
+        }
+
+        bool IsValidMove(List<IField> availableMoves, IField target)
+        {
+            return availableMoves.Contains(target);
         }
 
         public List<IField> GetAvailableMovesFor(Vector2Int position)
         {
-            List<Vector2Int> neighborsCoordinates = FindNeighborOfFieldCoordinates(position);
-            List<IField> availableMoves = new List<IField>();
-            foreach (var n in neighborsCoordinates)
+            List<IField> fieldsWithAvailableCoordinates = FindFieldsinDistance(position, _availableDistanceToMove);
+            List<IField> finalAvailableMoves = new List<IField>();
+
+            foreach (var n in fieldsWithAvailableCoordinates)
             {
-                var field = _board.GetField(n);
-                if (field != null || field.IsEmpty()) availableMoves.Add(field);
+                if (n == null || !n.IsEmpty()) continue;
+                if(n.Pawn ==null) finalAvailableMoves.Add(n);
             }
             return availableMoves;
         }
 
-        List<Vector2Int> FindNeighborOfFieldCoordinates(Vector2Int position)
+        List<IField> FindFieldsinDistance(Vector2Int position, int distance)
         {
-            List<Vector2Int> neighbors = new List<Vector2Int>();
-            neighbors.Add(new Vector2Int(position.x + 1, position.y));
-            neighbors.Add(new Vector2Int(position.x - 1, position.y));
-            neighbors.Add(new Vector2Int(position.x, position.y + 1));
-            neighbors.Add(new Vector2Int(position.x, position.y - 1));
-            neighbors.Add(new Vector2Int(position.x + 1, position.y + 1));
-            neighbors.Add(new Vector2Int(position.x + 1, position.y - 1));
-            neighbors.Add(new Vector2Int(position.x - 1, position.y + 1));
-            neighbors.Add(new Vector2Int(position.x - 1, position.y - 1));
-            return neighbors;
+            int length = distance * 2 + 1;
+            Vector2Int positionOfLeftDownAvailableField = new Vector2Int(position.x - distance, position.y - distance);
+            List<Vector2Int> availableCoordinates = new List<Vector2Int>();
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    Vector2Int availableCoordinate = positionOfLeftDownAvailableField + new Vector2Int(i,j);
+                    if (availableCoordinate == position) continue;
+                    availableCoordinates.Add(availableCoordinate);
+                }
+            }
+
+            return CoordinatesToFields(availableCoordinates);
+        }
+
+        List<IField> CoordinatesToFields(List<Vector2Int> coordinates)
+        {
+            var fields = from a in _board.GetAllFields()
+                        where coordinates.Contains(a.Position)
+                        select a;
+            return fields.ToList();
         }
 
     }
